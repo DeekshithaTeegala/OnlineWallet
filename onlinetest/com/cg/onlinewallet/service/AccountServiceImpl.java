29365509bb8dbb7deea69ef8b35739f796c13bb8
BVRIT <BVRIT@EEE-NEWLAB-228>
2020-02-19 15:11:40 +0530

    success

---
+++ onlinetest/com/cg/onlinewallet/service/AccountServiceImpl.java
@@ -0,0 +1,92 @@
+package com.cg.onlinewallet.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.cg.onlinewallet.bean.WalletAccount;
+import com.cg.onlinewallet.bean.WalletTransaction;
+import com.cg.onlinewallet.dao.AccountDao;
+import com.cg.onlinewallet.dao.AccountDaoMapImpl;
+import com.cg.onlinewallet.exception.*;
+
+public class AccountServiceImpl implements AccountService
+{
+	
+	private AccountDao accountDao;
+	public AccountServiceImpl() 
+	{
+		accountDao = new AccountDaoMapImpl();
+		
+	}
+    @Override
+	public void fundTransfer(int accountId1 , int accountId2 , double amount) throws AccountException
+	{
+		
+        
+	
+	}
+	@Override
+	public int createWalletAccount(WalletAccount account) throws AccountException
+	  {
+		Random random=new Random();
+		int accountId=random.nextInt(100)+1000;
+		
+
+
+		
+		account.setAccountId(accountId);
+	
+		return accountDao.createWalletAccount(account);
+	}
+
+	@Override
+	public double withdraw(int accountId, double amount) throws AccountException {
+		
+		
+		
+		WalletAccount walletAccount = accountDao.find(accountId);
+		WalletTransaction transaction = new WalletTransaction();
+		
+		List<WalletTransaction>   list = new ArrayList<WalletTransaction>();
+		list.add(transaction);
+		
+		transaction.setDescription(" amount withdrawn ");
+		
+		Random random=new Random();
+		int transactionId=random.nextInt(100)+1000;
+		transaction.setTransactionId(transactionId);
+		
+		walletAccount.addTransaction(transaction);
+		
+		return accountDao.withdraw(accountId,amount);
+		}
+		
+	
+
+
+
+	@Override
+	public double deposit(int accountId, double amount) throws AccountException
+	{
+		WalletAccount walletAccount = accountDao.find(accountId);
+		return 0;
+	}
+
+	@Override
+	public WalletAccount deleteAccount(int accountId) throws AccountException
+	{
+		WalletAccount walletAccount = accountDao.find(accountId);
+		
+		return accountDao.deleteAccount(accountId);
+		
+	}
+	public List<WalletTransaction> findAllTransaction(int accountId) throws AccountException
+	{
+		
+		WalletAccount walletAccount = accountDao.find(accountId);
+		return accountDao.findAllTransaction(accountId);
+	}
+	
+}
